a4cbd584c2adb24acd39f11ec0100aa8dda2ea51
Prince Kumar <127041212+princebeersingh@users.noreply.github.com>
2024-11-14 11:17:02 +0530

    Time: 46 ms (39.74%), Space: 64.8 MB (12.07%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -15,3 +15,10 @@ public:
         return pq.top();
     }
 };
+const static int __ = []()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr);                  
+    cout.tie(nullptr);               
+    return 0;
+}();
