281d2ecb65c0ea3874b7f0aebff1bb65dfdc4d47
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-02-05 20:27:35 +0530

    Time: 1 ms (65.42%), Space: 8.4 MB (45.16%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+ bool isSafe(vector<string>&board,int n,int i, int j)
+    {
+        if(i<0 || i>=n || j<0 || j>=n or board[i][j]=='Q')
+        {
+            return false;
+        }
+        // left 
+         // Check column
+    for (int x = 0; x < i; x++) {
+        if (board[x][j] == 'Q') {
+            return false;
+        }
+    }
+
+    // Check upper left diagonal
+    for (int x = i, y = j; x >= 0 && y >= 0; x--, y--) {
+        if (board[x][y] == 'Q') {
+            return false;
+        }
+    }
+
+    // Check upper right diagonal
+    for (int x = i, y = j; x >= 0 && y < n; x--, y++) {
+        if (board[x][y] == 'Q') {
+            return false;
+        }
+    }
+
+    return true;
+    }
+    void solveQueen(int n, int&ans,vector<string>&board,int row)
+    {
+        if(row==n)
+        {
+            ans+=1;
+            return ;
+        }
+        
+            for(int j=0;j<n;j++)
+            {
+                if(isSafe(board,n,row,j))
+                {
+                    board[row][j]='Q';
+                    solveQueen(n,ans,board,row +1);
+                    board[row][j]='.';
+                }
+            }
+        
+
+    }
+    int totalNQueens(int n) {
+        int ans=0;
+        std::vector<std::string> board(n, std::string(n, '.'));
+        solveQueen(n, ans, board, 0);
+        return ans;
+    }
+};
