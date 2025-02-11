e7b5bddb959871231cfd5e25daeb35f3bdba6c88
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-02-11 10:10:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (53.16%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -5,7 +5,7 @@ public:
         for(auto x:s)
         {
             if(!isdigit(x))ans+=x;
-            else if(ans.size()>0)ans.pop_back();
+            else ans.pop_back();
         }
         return ans;
     }
