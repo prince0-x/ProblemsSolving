fe0f62b39c290071156ad32562079432a14e17da
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-02-13 02:22:39 +0530

    Time: 32 ms (29.6%), Space: 149.2 MB (31.27%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,23 +1,34 @@
 class NumMatrix {
 public:
-    int row,col;
-    vector<vector<int>>sum;
-    NumMatrix(vector<vector<int>>& mat) {
-        row = mat.size();
-        col = mat[0].size();
-        sum.resize(row+1,vector<int>(col+1,0));
-        for(int i=1;i<=row;i++)
+   vector<vector<int>> mat;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m =matrix[0].size();
+        mat.resize(n,vector<int>(m,0));
+        mat[0][0]=matrix[0][0];
+        for(int i=1;i<n;i++)
         {
-            for(int j =1; j<=col;j++)
+            mat[i][0]=mat[i-1][0]+matrix[i][0];
+        }
+        for(int j=1;j<m;j++)
+        {
+            mat[0][j]=mat[0][j-1]+matrix[0][j];
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
             {
-                sum[i][j]= mat[i-1][j-1]+sum[i][j-1]+sum[i-1][j]-sum[i-1][j-1];
-                // cout<<sum[i][j];
+                mat[i][j]=matrix[i][j]+mat[i-1][j]+mat[i][j-1]-mat[i-1][j-1];
             }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return sum[row2+1][col2+1]+sum[row1][col1]-sum[row1][col2+1]-sum[row2+1][col1];
+        int ans = mat[row2][col2];
+        if(row1>0 and col1>0)ans +=mat[row1-1][col1-1];
+        if(row1>0)ans-=mat[row1-1][col2];
+        if(col1>0)ans-=mat[row2][col1-1];
+        return ans;
     }
 };
 
