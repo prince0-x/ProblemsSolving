0ea8ad5e92b2ab17dedbdf2767cb6435a74ba6b9
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-02-12 15:04:13 +0530

    Time: 0 ms (100%), Space: 115.8 MB (20.97%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>mini(n,0), maxi(n,0);
-        mini[0]=nums[0];
-        maxi[n-1]=nums[n-1];
-        for(int i=1;i<n;i++)
+        int v1 = INT_MAX, v2 = INT_MAX;
+        for(auto i:nums)
         {
-            mini[i]=min(mini[i-1], nums[i]);
-            // cout<<mini[i]<<" ";
+            if(i<=v1)
+            {
+                v1=i;
+            }
+            else if(i<=v2)
+            {
+                v2= i;
+            }
+            else{
+                return 1;
+            }
         }
-        // cout<<"\n";
-        for(int i=n-2;i>=0;i--)
-        {
-            maxi[i]=max(maxi[i+1], nums[i]);
-            // cout<<maxi[i]<<" ";
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>mini[i] and nums[i]<maxi[i]) return true;
-        }
-        return false;
+        return 0;
     }
 };
