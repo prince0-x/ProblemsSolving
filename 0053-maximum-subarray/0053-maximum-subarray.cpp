19c995b4e1bc75d2e9ac8b7ab15f28954c9d5155
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-02-25 23:47:59 +0530

    Time: 12 ms (5.19%), Space: 71.6 MB (97.7%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
 // kadanes algo
     int maxSubArray(vector<int>& nums) {
-        int sum = INT_MIN,sm = 0;
-        for(int i=0;i<nums.size();i++){
-            sm+=nums[i];
-            sum=max(sum,sm);
-            if(sm<0)
-            {
-                sm=0;
-            }
+        int sum=0, maxans =INT_MIN;
+        for(auto i: nums)
+        {
+            sum=max(sum+i, i);
+            maxans = max(maxans, sum);
         }
-        return sum;
+        return maxans;
     }
 };
