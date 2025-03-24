bd7ac2cafd475751f6f90c24b407fadbd4b81249
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-03-24 14:42:30 +0530

    Time: 75 ms (22.78%), Space: 84.1 MB (22.29%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+vector<vector<string>>res; 
+  bool ispalindrome(string s){
+    int n = s.size();
+    for(int i =0;i<n/2;i++){
+        if(s[i]!=s[n-i-1])return false;
+    }
+    return true;
+  }
+ 
+void sve(int index, string s, vector<string>&temp)
+{
+    if(index == s.size()){
+        res.push_back(temp);
+        return ;
+    }
+
+    for(int i = index;i<s.size();i++)
+    {
+        if(ispalindrome(s.substr(index, i-index+1))){
+            temp.push_back(s.substr(index, i-index+1));
+            sve(i+1,s, temp);
+            temp.pop_back();
+        }
+    }
+
+}
+    vector<vector<string>> partition(string s) {
+        vector<string>temp;
+        sve(0,s,temp);
+        return res;
+    }
+};
