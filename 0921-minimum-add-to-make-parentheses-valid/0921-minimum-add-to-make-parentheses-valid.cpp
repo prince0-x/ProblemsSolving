2d37411f57fb03d6c2a2f09232df014efac47443
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-03-27 21:51:36 +0530

    Time: 0 ms (100%), Space: 8.4 MB (81.04%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+       stack<char>st;
+       for(auto i:s)
+       {
+        if(i==')' &&!st.empty() && st.top() =='(')
+        {
+            st.pop();
+        }
+        else{
+            st.push(i);
+        }
+       } 
+       return st.size();
+    }
+};
