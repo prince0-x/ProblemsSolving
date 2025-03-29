144a1a33f29d78c96b4f6728d87bf708db8a7aa5
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-03-29 15:11:58 +0530

    Time: 7 ms (46.58%), Space: 9.8 MB (55.95%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -4,18 +4,18 @@ class Solution {
 public:
     int repeatedStringMatch(std::string a, std::string b) {
         std::string repeatA = "";
-        int count = 1;
+        int count = 0;
 
         while (repeatA.length() < b.length()) {
             repeatA += a;
             count++;
-            if (repeatA.find(b) != std::string::npos) return count-1;
+            if (repeatA.find(b) != std::string::npos) return count;
         }
 
         // One more repetition to check if b is fully contained
         repeatA += a;
         count++;
-        if (repeatA.find(b) != std::string::npos) return count-1;
+        if (repeatA.find(b) != std::string::npos) return count;
 
         return -1;
     }
