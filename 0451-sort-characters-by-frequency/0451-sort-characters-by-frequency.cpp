66f9df87338eb26ae62ab9508c6479b2560d2118
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-04-02 01:38:40 +0530

    Time: 1 ms (69.82%), Space: 11.4 MB (29.68%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,33 +2,43 @@ class Solution {
 public:
     string frequencySort(string s) {
         string ans = "";
-        vector<int> freq(52, 0); 
+        vector<int> freq(62, 0);
 
-    for (auto i : s) {
-        if (isupper(i)) {
-            freq[i - 'A' + 26]++; 
-        } else {
-            freq[i - 'a']++; 
+        for (auto i : s) {
+            if (isupper(i)) {
+                freq[i - 'A' + 26]++;
+            } else if (islower(i)) {
+                freq[i - 'a']++;
+            } else if (isdigit(i)) {
+                freq[i - '0' + 52]++;
+            }
         }
-    }
 
-    vector<pair<int, char>> freq_p;
-    for(int i =0;i<26;i++){
-        if (freq[i] > 0) {
-            freq_p.push_back({freq[i], i + 'a'}); 
-        }
-        if (freq[i + 26] > 0) {
-            freq_p.push_back({freq[i + 26], i + 'A'}); 
-        }
-    }
+        vector<pair<int, char>> freq_p;
 
-    sort(freq_p.begin(), freq_p.end(), [](const pair<int, char>& a, const pair<int, char>& b) {
-    return a.first > b.first;
-});
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                freq_p.push_back({freq[i], i + 'a'});
+            }
+            if (freq[i + 26] > 0) {
+                freq_p.push_back({freq[i + 26], i + 'A'});
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (freq[i + 52] > 0) {
+                freq_p.push_back({freq[i + 52], i + '0'});
+            }
+        }
+
+        sort(freq_p.begin(), freq_p.end(), [](const pair<int, char>& a, const pair<int, char>& b) {
+            return a.first > b.first;
+        });
 
         for (auto i : freq_p) {
-            ans += string(i.first, i.second ); 
+            ans += string(i.first, i.second);
         }
+
         return ans;
     }
 };
