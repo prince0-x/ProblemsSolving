56daec06f916936f7f57ef0eeef25fe3768cc965
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-04-09 02:38:33 +0530

    Time: 4 ms (47.51%), Space: 18.2 MB (44.27%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -5,27 +5,23 @@ public:
         for(auto f:flights){
             adj[f[0]].push_back({f[1], f[2]});
         }
-        queue<tuple<int, int, int>> q;
-        q.push({src, 0, 0});
-
-        vector<int> minCost(n, INT_MAX);
-        
-        minCost[src] = 0;
-
-        while (!q.empty()) {
-            auto [node, stops, cost] = q.front();
+        queue<pair<int, pair<int, int>>>q;
+        vector<int>dist(n,INT_MAX);
+        dist[src]=0;
+        q.push({0, {src, 0}}); // {step, node, weight}
+        while(!q.empty()){
+            int step = q.front().first;
+            int node = q.front().second.first;
+            int dis  = q.front().second.second;
             q.pop();
-
-            if (stops > k) continue;
-
-            for (auto [nbr, wt] : adj[node]) {
-                if (cost + wt < minCost[nbr]) {
-                    minCost[nbr] = cost + wt;
-                    q.push({nbr, stops + 1, cost + wt});
+            if(step >k)break;
+            for(auto [nbr, wtnbr]:adj[node]){
+                if(dis + wtnbr < dist[nbr]){
+                    dist[nbr]=dis+wtnbr;
+                    q.push({step+1, {nbr, wtnbr+dis}});
                 }
             }
         }
-
-        return minCost[dst] == INT_MAX ? -1 : minCost[dst];
+        return dist[dst]==INT_MAX?-1:dist[dst];
     }
 };
