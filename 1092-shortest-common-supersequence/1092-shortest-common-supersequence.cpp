b1835cd38faa3e158c96fc9621ce1c7a79c407a6
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-04-16 15:49:01 +0530

    Time: 27 ms (35.92%), Space: 29 MB (8.87%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,44 +1,61 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int m = str1.size();
-        int n = str2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        int n = str1.size(), m = str2.size();
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        for(int i =1;i<=n;i++){
+            for(int j =1; j<=m;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-        int i = m, j = n;
-        string res = "";
-        while(i>0 and j>0)
-        {
-            if(str1[i-1]==str2[j-1])
-            {
-                res+=str1[i-1];i--;j--;
-            }
-            else if(dp[i-1][j]>dp[i][j-1])
-            {
-                res+=str1[i-1];i--;
+        int i =n, j=m;
+        string lcs="", ans="";
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                lcs=str1[i-1]+lcs;
+                i--;
+                j--;
             }
             else{
-                res+=str2[j-1];j--;
+                if(dp[i-1][j]>dp[i][j-1]){
+                  i--;
+                }
+                else {
+                    j--;
+                }
             }
+            
+        } 
+        int k=0;
+        i=0, j =0;
+        while(k<lcs.size()){
+            while(i<n && k< lcs.size() && lcs[k]!=str1[i]){
+                ans+=str1[i];
+                i++;
+            }
+            while(j<m && k< lcs.size() && lcs[k]!=str2[j]){
+                ans+=str2[j];
+                j++;
+            }
+            ans+=lcs[k];
+            k++;
+            i++;
+            j++;
         }
 
-        while(i>0)
-        {
-            res+=str1[i-1];i--;
+
+        while(i<n){
+            ans+=str1[i];
+            i++;
         }
-        while(j>0)
-        {
-            res+=str2[j-1];j--;
+        while(j<m){
+            ans+=str2[j];
+            j++;
         }
-        reverse(res.begin(), res.end());
-        return res;
+        return ans;
     }
 };
