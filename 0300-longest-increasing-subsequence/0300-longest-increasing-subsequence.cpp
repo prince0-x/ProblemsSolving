0f2fe35dfabe1871266da20c2d4fd904d195c61f
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-04-18 15:20:06 +0530

    Time: 8 ms (76.01%), Space: 14.1 MB (87.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -12,8 +12,13 @@ int solve(vector<int>&nums, int index, int prevind, vector<vector<int>>&dp){
     return dp[index][prevind+1]= max(take, notTake);
 }
     int lengthOfLIS(vector<int>& nums) {
-       if(nums.size()==0)return 0;
-       vector<vector<int>>dp(nums.size(), vector<int>(nums.size()+1, -1));
-       return solve(nums,0,-1 , dp); 
+       vector<int>ans;
+       for(auto i :nums){
+          int ind = lower_bound(ans.begin(), ans.end(), i)-ans.begin();
+          if(ind != ans.size())ans[ind] = i;
+          else ans.push_back(i);
+       }
+       for(auto i:ans)cout<<i<<" ";
+       return ans.size();
     }
 };
