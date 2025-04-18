0571576230dd2e55d8a5673f5590d8931b66c80b
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-04-18 15:43:39 +0530

    Time: 71 ms (51.74%), Space: 14.7 MB (37.1%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-int solve(vector<int>&nums, int index, int prevind, vector<vector<int>>&dp){
-    if(index == nums.size())return 0;
-    if(dp[index][prevind+1]!=-1)return dp[index][prevind+1];
-    int notTake = solve(nums, index+1, prevind, dp);
-    int take = 0;
-    if(prevind ==-1 || nums[index]>nums[prevind] )
-    {
-        take = 1+solve(nums, index+1, index, dp);
-    }
-    return dp[index][prevind+1]= max(take, notTake);
-}
     int lengthOfLIS(vector<int>& nums) {
-       vector<int>ans;
-       for(auto i :nums){
-          int ind = lower_bound(ans.begin(), ans.end(), i)-ans.begin();
-          if(ind != ans.size())ans[ind] = i;
-          else ans.push_back(i);
-       }
-       for(auto i:ans)cout<<i<<" ";
-       return ans.size();
+      int n = nums.size();
+      vector<int>dp(n, 1), par(n), lis;
+      int index = 0, ans=1;
+      for(int i =1; i<n;i++){
+            for(int j =0; j<i;j++){
+                  if(nums[i]> nums[j]){
+                        if(dp[i]< 1+dp[j]){
+                            dp[i]=1+dp[j];
+                            par[i]=j;
+                        }
+                  }
+            }
+            if(dp[i]>ans){
+                ans = dp[i];
+                index= i;
+            }
+      }
+      
+      
+      while(par[index]!=index){
+        lis.push_back(nums[index]);
+        index = par[index];
+      }
+      lis.push_back(nums[index]);
+      for(auto i:lis)cout<<i<<" ";
+      
+      return *max_element(dp.begin(), dp.end());
     }
 };
