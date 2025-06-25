545bf8a0c7e24a93e82592e4b0cec8e7f7803c51
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-06-25 11:11:43 +0530

    Time: 21 ms (47.99%), Space: 46.3 MB (65.76%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+public:
+    vector<int>parent;
+    int find_(int a){
+        if(parent[a]!= a){
+            return parent[a] = find_(parent[a]);
+        }
+        return parent[a];
+    }
+    int union_( int a, int b){
+        int roota = find_(a);
+        int rootb = find_(b);
+        if(roota == rootb){
+            return 1;
+        }
+        parent[rootb] = roota;
+        return 0;
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        parent.resize(n);
+        for(int i =0;i<n;i++){
+            parent[i]=i;
+        }
+        int components = 0, extracable = 0;
+        for(auto c : connections){
+            if(union_(c[0], c[1])){
+                extracable++;
+            }
+        }
+        for(int i =0;i<n;i++){
+            if(parent[i]==i){
+                components++;
+            }  
+        }
+        cout<<extracable << " "<<components;
+        if(extracable >= components - 1)return components-1;
+
+        return -1;
+    }
+};
