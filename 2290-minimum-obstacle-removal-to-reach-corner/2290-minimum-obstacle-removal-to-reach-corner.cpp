690bf3962698c5577edf26d6fee5d64bb3905a3b
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-06-25 14:27:30 +0530

    Time: 1366 ms (5.03%), Space: 356.2 MB (5.38%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        auto issafe = [&](int a, int b){
+            return a>=0 && a<n && b>=0 && b<m;
+        };
+        vector<vector<int>>directions = {{1, 0}, {0,1}, {-1, 0}, {0, -1}};
+        vector<vector<int>>dist(n, vector<int>(m, INT_MAX));
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>>pq;
+        pq.push({0, 0, 0});
+        while(!pq.empty()){
+            auto [wt, x, y] = pq.top();
+            pq.pop();
+            if(x==n-1 && y == m-1)return wt;
+
+            for(auto dir:directions){
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+
+                if(issafe(nx, ny) && wt+grid[nx][ny] < dist[nx][ny]){
+                    dist[nx][ny] = wt+grid[nx][ny];
+                    pq.push({wt+grid[nx][ny], nx, ny});   
+                }
+            }
+        }
+        return -1;
+    }
+};
