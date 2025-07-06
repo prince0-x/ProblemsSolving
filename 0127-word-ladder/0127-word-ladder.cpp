79fc92fb5431e19b7474e9b990d98f0f8681ae9f
Prince Kumar <127041212+priincce@users.noreply.github.com>
2025-07-06 23:03:16 +0530

    Time: 138 ms (41.51%), Space: 22.7 MB (25.42%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    int ladderLength(string startWord, string targetWord,
-                         vector<string>& wordList) {
-        // Code here
-        set<string>s(wordList.begin(), wordList.end()), vis;
-        queue<string>q;
-        q.push(startWord);
-        vis.insert(startWord);
-        int ans =0;
+    int ladderLength(string startWord, string endWord, vector<string>& wordList) {
+        unordered_set<string>vis, s(wordList.begin(), wordList.end());
+        queue<pair<string,int>>q;
+        q.push({startWord,1});
         while(!q.empty()){
-            ans++;
             int siz = q.size();
-            for(int i =0; i<siz; i++){
-                string word = q.front();
-                if(word ==targetWord)return ans;
+            for(int i=0;i<siz;i++){
+                auto f = q.front();
+                
+                string word = f.first;
+                int step = f.second;
+                if(word==endWord){
+                    return step;
+                }
                 q.pop();
-                for(int i=0;i<word.size();i++){
+                
+                for(int j=0;j<word.size();j++){
                     string temp = word;
-                    for(char j ='a'; j<='z'; j++)
-                    {
-                        temp[i]=j;
-                        if(s.find(temp)!=s.end() && !(vis.find(temp)!=vis.end()))
-                        {
-                            q.push(temp);
+                    for(char ch='a';ch<='z';ch++){
+                        temp[j]=ch;
+                        if(vis.find(temp)==vis.end()&& s.find(temp)!=s.end()){
                             vis.insert(temp);
+                            q.push({temp, step+1});
                         }
                     }
                 }
             }
-            
         }
         return 0;
     }
