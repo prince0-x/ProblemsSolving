8e8684f7b7d07ad02a6df2ad5391c66f4fae422b
Prince Kumar <127041212+prince0-x@users.noreply.github.com>
2026-06-17 11:29:57 +0530

    1696-jump-game-vi

--- LeetCode/Medium/1696-jump-game-vi/1696-jump-game-vi.cpp
+++ LeetCode/Medium/1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -6,10 +6,15 @@ int n;
     int maxResult(vector<int>& nums, int k) {
         n = nums.size();
         dp.assign(n, INT_MIN);
-        multiset<int>s({dp[0]=nums[0]});
+        deque<int>dq{0};
+        dp[0]= nums[0];
         for(int i=1; i<n; i++){
-            if(i>k)s.erase(s.find(dp[i-k-1]));
-            s.insert(dp[i]=*rbegin(s) +nums[i]);
+            if(dq.front() < i-k)dq.pop_front();
+            dp[i]= nums[i]+dp[dq.front()];
+            while(!dq.empty() && dp[dq.back()] <= dp[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
         }
         return dp.back();
     }
